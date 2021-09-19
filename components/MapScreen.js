f1d27534c18a1fb3db1b89a46034da4319f18ad1
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-09-19 22:30:05 +0800

    Update MapScreen.js

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,5 +1,5 @@
 export default function MapScreen() {
     return (
-        <iframe src="https://storymaps.arcgis.com/stories/a39d121644884086a007e4f01d63829e" width="100%" height="500px" frameBorder="0" allowFullScreen allow="geolocation"></iframe>
+        <iframe src="https://storymaps.arcgis.com/stories/842aacb0704c4d11b22a8dc94d270b50" width="100%" height="500px" frameBorder="0" allowFullScreen allow="geolocation"></iframe>
     );
 }
