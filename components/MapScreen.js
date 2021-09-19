8e804ecadf64a8d1a844f42e04c94df832e108d5
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-09-19 23:40:40 +0800

    iframe full height

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,5 +1,5 @@
 export default function MapScreen() {
     return (
-        <iframe src="https://storymaps.arcgis.com/stories/842aacb0704c4d11b22a8dc94d270b50" width="100%" height="500px" frameBorder="0" allowFullScreen allow="geolocation"></iframe>
+        <iframe src="https://storymaps.arcgis.com/stories/842aacb0704c4d11b22a8dc94d270b50" width="100%" style={{height: '100vh', position: 'fixed'}} frameBorder="0" allowFullScreen allow="geolocation"></iframe>
     );
 }
